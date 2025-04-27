838a24c38b4bf8dbaea4f04c98c13c60e6b48f34
juanro03 <roldanjuan2003@gmail.com>
2025-04-27 11:03:41 -0300

    creación proyecto

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('api/auth/', include('dj_rest_auth.urls')),  # Login, logout
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),  # Registro
     path('admin/', admin.site.urls),
 ]
