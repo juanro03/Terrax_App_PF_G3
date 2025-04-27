e23788cda7d3d4daca2201fc94d3a99faf17e70f
juanro03 <roldanjuan2003@gmail.com>
2025-04-27 19:09:23 -0300

    conexión con postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,8 +91,12 @@ AUTHENTICATION_BACKENDS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'terrax_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
