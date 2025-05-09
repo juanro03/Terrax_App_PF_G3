547557c66db8d08775af64fbd3d4120f5840d8de
15agusgarcia <agustinagarciamercado@hotmail.com>
2025-05-08 23:20:37 -0300

    Creacion de rama iniciar sesion -- Frontend OK para iniciar sesión

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,56 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/Inicio/Sidebar";
 import UserCrud from "./components/Usuarios/UserCrud";
 import CampoCRUD from "./components/campos/campos";
-import './components/campos/styles.css';
 import VerCampos from "./components/campos/ver_campos";
-
-
+import Login from "./components/Auth/Login";
+import PrivateRoute from "./components/Auth/PrivateRoute";
+import "./components/campos/styles.css";
 
 function App() {
   const location = useLocation();
+  const hideSidebarRoutes = ["/login"];
 
   return (
     <div className="d-flex">
-      <Sidebar />
-      <div className="flex-grow-1 p-4 bg-white" style={{ height: "100vh", overflowY: "auto" }}>
+      {!hideSidebarRoutes.includes(location.pathname) && <Sidebar />}
+      <div
+        className="flex-grow-1 p-4 bg-white"
+        style={{ height: "100vh", overflowY: "auto" }}
+      >
         <Routes location={location}>
-          <Route path="/usuarios" element={<UserCrud />} />
-          <Route path="/campos" element={<CampoCRUD/>} />
-          <Route path="/" element={<div />} />
-          <Route path="/ver-campos" element={<VerCampos />} />
-          {/* otras rutas */}
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/usuarios"
+            element={
+              <PrivateRoute>
+                <UserCrud />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/campos"
+            element={
+              <PrivateRoute>
+                <CampoCRUD />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/ver-campos"
+            element={
+              <PrivateRoute>
+                <VerCampos />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <UserCrud />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </div>
@@ -28,4 +60,3 @@ function App() {
 }
 
 export default App;
-
